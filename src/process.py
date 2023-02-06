b7338b5bfa7f4a9f8340722cfc2bdc36f6b66d32
Freddy <f5wang@ucsd.edu>
2023-02-05 23:58:27 -0800

    filter

--- src/process.py
+++ src/process.py
@@ -6,9 +6,18 @@ from collections import Counter
 
 from louvain import Louvain
 
-def getComms(graph):
+def topComms(graph):
     communities = Louvain(graph).predict()
     counts = Counter(communities.values())
     sorted = counts.most_common()
-    return sorted
+    topLabels = [i[0] for i in sorted[:10]]
+    return topLabels
+
+def nodeFilter(graph, label):
+    nodes = (node
+    for node, data
+    in graph.nodes(data=True)
+    if data.get("detected") == label)
+    return graph.subgraph(nodes)
+
 
