e9926670a88d7d56160b150a025194c192002ff9
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2023-02-19 18:37:17 -0800

    Clear out repo for website

--- src/louvain.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import community
-
-class Louvain:
-    def __init__(self, G: nx.graph) -> None:
-        self.G = G.copy()
-        
-    def predict(self):
-        return community.best_partition(self.G)
--- src/twitter_traverse_results/README.md
+++
@@ -1,3 +0,0 @@
-# `twitter_traverse_results`
-
-When you run the twitter_traverse code, the data will be generated here.
